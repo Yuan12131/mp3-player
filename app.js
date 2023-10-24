36d8ee071d39dc56579a279b81655af389d81113
leeyuan <dbdks32123@gmail.com>
2023-10-24 15:41:13 +0900

    요청 method GET, URL 설정

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer(function(request, require){
 
-  let writeHeadObject = {
-    'Content-Type' : 'text-html'
+  if (req.method === 'GET'&& req.url === '/'){
+    response.writeHead(200, {'Content-Type' : 'text-html'})
+    fs.readFile("./static/mp3.html", function(err, data){
+      if(err){
+        console.error("파일을 읽지 못했습니다.")
+      } else {
+        response.end(data)
+      }
+    })
   }
-
-  response.writeHead(200, writeHeadObject)
-
-  fs.readFile("./static/mp3.html", function(err, data){
-    if(err){
-      console.error("파일을 읽지 못했습니다.")
-    } else {
-      response.end(data)
-    }
-  })
 }).listen(8019);
