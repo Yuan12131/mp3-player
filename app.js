6c5ce6db0faf69524684392ff3c448d6ba275008
leeyuan <dbdks32123@gmail.com>
2023-10-24 15:45:31 +0900

    서버 가동 확인

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer(function(request, require){
+http.createServer(function(req, res){
 
   if (req.method === 'GET'&& req.url === '/'){
-    response.writeHead(200, {'Content-Type' : 'text-html'})
+    res.writeHead(200, {'Content-Type' : 'text-html'})
     fs.readFile("./static/mp3.html", function(err, data){
       if(err){
         console.error("파일을 읽지 못했습니다.")
       } else {
-        response.end(data)
+        res.end(data)
       }
     })
   }
-}).listen(8019);
+}).listen(`${8080}`, (err) => {
+  console.log(`서버 가동중 확인 -> http://localhost:${8080}/`);
+});
