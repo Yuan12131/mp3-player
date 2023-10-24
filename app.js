3b45a1e2c1535c6fec9aa18d1c4a8c78bf267fc7
leeyuan <dbdks32123@gmail.com>
2023-10-24 19:41:53 +0900

    mp3파일 연결

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path'); //Node.js에서 파일 및 디렉토리 경로를 처리하는 데 사용되는 내장 모듈인 "path" 모듈을 가져온 것
 
 http.createServer(function(req, res){
 
@@ -22,11 +23,21 @@ http.createServer(function(req, res){
         res.end(data);
       }
     });
+  } else if (req.url === '/songOne.mp3' || req.url === '/songTwo.mp3' || req.url === '/songThree.mp3') {
+    // MP3 파일에 대한 요청 처리 (MP3 파일을 스트리밍으로 제공)
+    const filePath = path.join(__dirname, req.url.slice(1)); // 파일 경로 생성
+    const stat = fs.statSync(filePath);
+    res.writeHead(200, {
+      'Content-Type': 'audio/mpeg',
+      'Content-Length': stat.size,
+    });
+    const readStream = fs.createReadStream(filePath);
+    readStream.pipe(res);
   } else {
     // 알 수 없는 요청에 대한 응답 처리
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
-}).listen(`${8080}`, (err) => {
-  console.log(`서버 가동중 확인 -> http://localhost:${8080}/`);
+}).listen(`${8019}`, (err) => {
+  console.log(`서버 가동중 -> http://localhost:${8019}/`)
 });
