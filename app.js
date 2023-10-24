d7b4874c51763a16a7fdab7a18de4f9d13440cbe
leeyuan <dbdks32123@gmail.com>
2023-10-24 15:39:16 +0900

    서버에서 html 불러오기

--- app.js
+++ app.js
@@ -2,4 +2,17 @@ const http = require('http');
 
 http.createServer(function(request, require){
 
+  let writeHeadObject = {
+    'Content-Type' : 'text-html'
+  }
+
+  response.writeHead(200, writeHeadObject)
+
+  fs.readFile("./static/mp3.html", function(err, data){
+    if(err){
+      console.error("파일을 읽지 못했습니다.")
+    } else {
+      response.end(data)
+    }
+  })
 }).listen(8019);
