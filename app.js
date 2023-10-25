2c4552f0a8b60b09cc1f4ce2e8d85e7ff7951b2c
leeyuan <dbdks32123@gmail.com>
2023-10-25 10:34:17 +0900

    주석 추가

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path'); //Node.js에서 파일 및 디렉토리 경로를 처리하는 데 사용되는 내장 모듈인 "path" 모듈을 가져온 것
+const path = require('path'); //Node.js에서 파일 및 디렉토리 경로를 처리하는 데 사용되는 내장 모듈인 "path"
 
 http.createServer(function(req, res){
 
@@ -14,7 +14,7 @@ http.createServer(function(req, res){
       }
     })
   } else if (req.url === '/style.css') {
-    // CSS 파일에 대한 요청 처리 (CSS 파일을 제공)
+    // CSS 파일에 대한 요청 처리
     res.writeHead(200, { 'Content-Type': 'text/css' });
     fs.readFile('./static/style.css', function(err, data) {
       if (err) {
@@ -24,14 +24,24 @@ http.createServer(function(req, res){
       }
     });
   } else if (req.url === '/songOne.mp3' || req.url === '/songTwo.mp3' || req.url === '/songThree.mp3') {
-    // MP3 파일에 대한 요청 처리 (MP3 파일을 스트리밍으로 제공)
-    const filePath = path.join(__dirname, req.url.slice(1)); // 파일 경로 생성
+    // MP3 파일에 대한 요청 처리
+
+    // 디렉토리 경로와 파일 이름을 조합하여 최종 파일의 경로를 만듦 -> 실제로 서버에서 찾아야 하는 MP3 파일의 경로
+    const filePath = path.join(__dirname, req.url.slice(1)); 
+
+    // statSync : Node.js에서 파일 또는 디렉토리의 정보(메타데이터)를 동기적으로 읽어오는 메서드
     const stat = fs.statSync(filePath);
     res.writeHead(200, {
       'Content-Type': 'audio/mpeg',
       'Content-Length': stat.size,
     });
+    
+    // 파일을 조각조각 읽어오고, 응답으로 전송하는 과정을 처리
+
+    // Node.js의 파일 시스템 모듈인 fs를 사용하여 파일을 읽는 스트림을 생성
     const readStream = fs.createReadStream(filePath);
+
+    // 데이터를 처리한 후, 이를 원하는 곳으로 보내고 싶을 때 스트림을 pipe → 파일 스트림의 데이터를 응답 스트림(res)으로 보내는 것
     readStream.pipe(res);
   } else {
     // 알 수 없는 요청에 대한 응답 처리
